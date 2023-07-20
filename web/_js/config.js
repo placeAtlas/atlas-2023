6ebbda89e1a3610f6adca75ac53bc96ecfb8639c
Hans5958 <hans5958@outlook.com>
2023-07-21 00:36:47 +0700

    Add 17:30 canvas

--- web/_js/config.js
+++ web/_js/config.js
@@ -55,6 +55,7 @@ const variationsConfig = {
 			{ "timestamp": 1689868839, "url": ["./_img/canvas/main/1689868839.png"] },
 			{ "timestamp": 1689870611, "url": ["./_img/canvas/main/1689870611.png"] },
 			{ "timestamp": 1689872431, "url": ["./_img/canvas/main/1689872431.png"] },
+			{ "timestamp": 1689874200, "url": ["./_img/canvas/main/1689874200.png"] },
 		],
 		icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 192 192" aria-hidden="true"><polygon points="154 0 154 38 39 38 39 192 0 192 0 0"/><polygon points="192 38 192 192 77 192 77 153 154 153 154 38"/><rect x="77" y="77" width="38" height="38"/></svg>'
 	},
