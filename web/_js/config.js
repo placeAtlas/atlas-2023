ada37c864bb0822a0491f1ad6ccc16ec4fbe1bfe
Hans5958 <hans5958@outlook.com>
2023-07-20 22:43:51 +0700

    Add 15:30 canvas

--- web/_js/config.js
+++ web/_js/config.js
@@ -39,10 +39,10 @@ const variationsConfig = {
 	default: {
 		name: "r/place",
 		code: "",
-		default: 4,
-		drawablePeriods: [1, 4],
+		default: 5,
+		drawablePeriods: [1, 5],
 		drawableRegions: [
-			[[0, 4], [-500, -500, 499, 499]],
+			[[0, 5], [-500, -500, 499, 499]],
 		],
 		// expansions: [],
 		versions: [
@@ -51,6 +51,7 @@ const variationsConfig = {
 			{ "timestamp": 1689861680, "url": ["./_img/canvas/main/1689861680.png"] },
 			{ "timestamp": 1689863453, "url": ["./_img/canvas/main/1689863453.png"] },
 			{ "timestamp": 1689865307, "url": ["./_img/canvas/main/1689865307.png"] },
+			{ "timestamp": 1689867430, "url": ["./_img/canvas/main/1689867430.png"] },
 		],
 		icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 192 192" aria-hidden="true"><polygon points="154 0 154 38 39 38 39 192 0 192 0 0"/><polygon points="192 38 192 192 77 192 77 153 154 153 154 38"/><rect x="77" y="77" width="38" height="38"/></svg>'
 	},
