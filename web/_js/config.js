33c39192d52affc4a740eb49055fd8f7f67a15a6
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2023-07-21 23:07:11 -0400

    Updated canvas

--- web/_js/config.js
+++ web/_js/config.js
@@ -123,6 +123,7 @@ const variationsConfig = {
 			{ "timestamp": 1689989422, "url": ["./_img/canvas/main/1689989422.png"] },
 			{ "timestamp": 1689991244, "url": ["./_img/canvas/main/1689991244.png"] },
 			{ "timestamp": 1689993006, "url": ["./_img/canvas/main/1689993006.png"] },
+			{ "timestamp": 1689994829, "url": ["./_img/canvas/main/1689994829.png"] },
 		],
 		icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 192 192" aria-hidden="true"><polygon points="154 0 154 38 39 38 39 192 0 192 0 0"/><polygon points="192 38 192 192 77 192 77 153 154 153 154 38"/><rect x="77" y="77" width="38" height="38"/></svg>'
 	},
