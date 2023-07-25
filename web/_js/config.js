94ab89b87d7167629ebb26b800cba08721790562
Hans5958 <hans5958@outlook.com>
2023-07-25 19:01:42 +0700

    Add 11:00-11:30 canvasses

--- web/_js/config.js
+++ web/_js/config.js
@@ -288,6 +288,8 @@ const variationsConfig = {
 			{ "timestamp": 1690277401, "url": ["./_img/canvas/main/1690277401.png"] },
 			{ "timestamp": 1690279200, "url": ["./_img/canvas/main/1690279200.png"] },
 			{ "timestamp": 1690281000, "url": ["./_img/canvas/main/1690281000.png"] },
+			{ "timestamp": 1690282800, "url": ["./_img/canvas/main/1690282800.png"] },
+			{ "timestamp": 1690284600, "url": ["./_img/canvas/main/1690284600.png"] },
 		],
 		icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 192 192" aria-hidden="true"><polygon points="154 0 154 38 39 38 39 192 0 192 0 0"/><polygon points="192 38 192 192 77 192 77 153 154 153 154 38"/><rect x="77" y="77" width="38" height="38"/></svg>'
 	},
