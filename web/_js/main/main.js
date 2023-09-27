4502ccbd58a94c7a3464589ee70ef2dc129723c2
Hans5958 <hans5958@outlook.com>
2023-09-27 14:53:42 +0700

    Adjust global top padding when resizing window

--- web/_js/main/main.js
+++ web/_js/main/main.js
@@ -537,6 +537,10 @@ const noticeEl = document.querySelector("#headerNotice")
 const noticeButton = noticeEl.querySelector('[role=button]')
 const noticeText = noticeEl.querySelector('p').textContent.trim()
 
+const resizeGlobalTopPadding = () => {
+	document.body.style.setProperty("--global-top-padding", noticeEl.offsetHeight + 'px')
+}
+
 if (window.localStorage.getItem('announcement-closed')) {
 	window.localStorage.setItem('closed-notice', window.localStorage.getItem('announcement-closed'))
 	window.localStorage.removeItem('announcement-closed')
@@ -547,9 +551,11 @@ if (noticeText && noticeText !== window.localStorage.getItem('closed-notice')) {
 	setTimeout(() => {
 		document.body.style.setProperty("--global-top-padding", noticeEl.offsetHeight + 'px')
 	}, 500)
+	window.addEventListener('resize', resizeGlobalTopPadding)
 }
 
 noticeEl.querySelector('[role=button]').addEventListener('click', () => {
 	window.localStorage.setItem('closed-notice', noticeText)
+	window.removeEventListener('resize', resizeGlobalTopPadding)
 	document.body.style.setProperty("--global-top-padding", null)
 })
