e8a120885d181f88df455b1a6e37f0028191b30b
Stefano <stefano@stefanocoding.me>
2025-03-14 02:05:52 +0000

    Update modal title format to include entry ID for clarity

--- web/_js/main/infoblock.js
+++ web/_js/main/infoblock.js
@@ -209,7 +209,7 @@ function createModal(entry) {
 		<div class="modal-dialog modal-lg">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title">Comments for ${entry.name}</h5>
+					<h5 class="modal-title">Commenting on: ${entry.name} (${entry.id})</h5>
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body">
